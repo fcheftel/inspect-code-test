1a3d44b19d585b6e81cfe6bf6d9fd4027846b866
Fabien Cheftel <fabien@struckd.com>
2023-10-18 10:54:51 +0200

    edit const value

--- Assets/Scripts/ThisIsOnlyATest.cs
+++ Assets/Scripts/ThisIsOnlyATest.cs
@@ -8,7 +8,7 @@ public class ThisIsOnlyATest : MonoBehaviour
 
     [SerializeField] private string test2;
     
-    private const float test3 = 1f;
+    private const float test3 = 2f;
     
     // Start is called before the first frame update
     void Start()
