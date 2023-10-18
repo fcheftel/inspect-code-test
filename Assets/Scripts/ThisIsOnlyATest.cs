66c10466c754d6b62097fe3dac4ace7859de2e1e
Fabien Cheftel <fabien@struckd.com>
2023-10-18 10:52:31 +0200

    setup sample unity project

---
+++ Assets/Scripts/ThisIsOnlyATest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThisIsOnlyATest : MonoBehaviour
+{
+    private int test = 0;
+
+    [SerializeField] private string test2;
+    
+    private const float test3 = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (true)
+            Debug.Log("This is only a test");
+    }
+}
